ce794d9708ecabfd09f2aee1da78a203b2960ecb
Saynok <saynokdeveloper@gmail.com>
2022-09-12 15:10:07 +0300

    реализован автомат мигания светодиодом

--- src/led.hpp
+++ src/led.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <Arduino.h>
+#include <Ticker.h>
 
 enum LED_Settings
 {
@@ -7,59 +8,106 @@ enum LED_Settings
 };
 
 /* первое число в строке - время горения в миллисекундах, второе - время отключенного состояния светодиода
+Размер последовательности должен быть четным.
 
 Пример:
-int _led_default_mode[] =
+int _LED_DEFAULT_MODE[] =
     {500, 500};
 Результат: Поочередное мигание каждые 500мс
 
-int _led_default_mode[] =
+int _LED_DEFAULT_MODE[] =
     {100, 900};
 Результат: Коротко зажигается на 100 миллисекунд 1 раз в секунду (100мс + 900мс = 1000мс = период 1с).
 
-int _led_default_mode[] =
+int _LED_DEFAULT_MODE[] =
     {500, 500,
      100, 900};
 Результат: Длительное горение сменяется короткой вспышкой
-
 */
-int _led_default_mode[] =
+#define LED_Set_blink_mode(mode) _LED_Set_blink_mode(mode, sizeof(mode));
+
+enum LED_FSM
+{
+    LED_STATE_NOT_INITIALIZED,
+    LED_STATE_BUSY,
+    LED_STATE_CHANGE,
+};
+
+unsigned int _LED_DEFAULT_MODE[] =
     {0, 0};
 
-int *_led_current_mode = NULL;
-bool led_initialized = false;
+LED_FSM _led_state = LED_STATE_NOT_INITIALIZED;
+unsigned int _current_timing = 0;
+Ticker _ticker;
+unsigned int *_led_current_mode = NULL;
+unsigned int _led_current_mode_size = 0;
 
-void LED_Set_blink_mode(int *mode)
+void _LED_Set_blink_mode(unsigned int *mode, unsigned int mode_size)
 {
-    if (!led_initialized)
+    if (_led_state == LED_STATE_NOT_INITIALIZED)
     {
         Serial.println("\nERROR\nСветодиоды не инициализированы!\n");
         return;
     }
+    if (_led_current_mode == mode)
+        return;
+    _current_timing = 0;
     _led_current_mode = mode;
+    _led_current_mode_size = mode_size / 4; // int занимает 4 байта
+    digitalWrite(LED_PIN, 0);
 }
+
 void LED_Setup()
 {
-    if (led_initialized)
+    if (_led_state != LED_STATE_NOT_INITIALIZED)
     {
         return;
     }
-    led_initialized = true;
-
     pinMode(LED_PIN, OUTPUT);
-    LED_Set_blink_mode(_led_default_mode);
+    _led_state = LED_STATE_CHANGE;
+    LED_Set_blink_mode(_LED_DEFAULT_MODE);
+}
+
+void LED_Ticker_Estimated()
+{
+    _led_state = LED_STATE_CHANGE;
 }
 
 void LED_Quant()
 {
-    if (!led_initialized)
+    switch (_led_state)
     {
-        return;
+    case LED_STATE_NOT_INITIALIZED:
+    case LED_STATE_BUSY:
+        break;
+
+    case LED_STATE_CHANGE:
+        if (_led_current_mode == NULL || _led_current_mode_size == 0)
+            _led_state = LED_STATE_BUSY;
+
+        if (_current_timing == _led_current_mode_size)
+        {
+            _current_timing = 0;
+        }
+
+        digitalWrite(LED_PIN, (_current_timing % 2) == 1);
+        _ticker.attach(_led_current_mode[_current_timing] / 1000.0f, &LED_Ticker_Estimated);
+
+        // Serial.print("time\t");
+        // Serial.print(_led_current_mode[_current_timing] / 1000.0f);
+        // Serial.print("\tstate\t");
+        // Serial.print((_current_timing % 2) == 0);
+        // Serial.print("\t_current_timing\t");
+        // Serial.print(_current_timing);
+        // Serial.print("\tsize of mode\t");
+        // Serial.println(_led_current_mode_size);
+
+        if (_current_timing < _led_current_mode_size)
+        {
+            _current_timing++;
+        }
+
+        _led_state = LED_STATE_BUSY;
+        break;
     }
-
-    /*
-
-    TODO: Автомат переключения светодиодов в соответствии с режимом
-
-    */
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 // первое число в строке - время горения в миллисекундах, второе - время отключенного состояния светодиода
 // ТЗ: "встроенный светодиод мигает 3 раза 20мс с интервалом 300мс, 2 раза 500/500 и далее по кругу"
-int led_our_mode[] =
+unsigned int LED_OUR_MODE[] =
     {20, 300,
      20, 300,
      20, 300,
@@ -20,9 +20,8 @@ void setup()
 
   Buttons_Setup();
 
-  LED_Set_blink_mode(led_our_mode);
   LED_Setup();
-  LED_Set_blink_mode(led_our_mode);
+  LED_Set_blink_mode(LED_OUR_MODE);
 
   WIFI_Start_time_from_server();
   WIFI_Setup();
@@ -40,12 +39,18 @@ void loop()
 
   // ТЗ: кнопка встроенная реализовать короткое и длительное нажатие с выводом в порт - "short" и "long"
   if (Buttons_User_Short())
+  {
     Serial.println("Short");
+  }
 
   if (Buttons_User_Long())
+  {
     Serial.println("Long");
+  }
 
   // ТЗ: "подключиться по wifi, получить время, выводить его в консоль раз в 3 сек"
   if (WIFI_Time_Updated())
+  {
     WIFI_Print_current_time();
+  }
 }
